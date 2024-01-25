2acac64a99515144c0cfa749ba3802ebe2cbaefb
Scotty <scottmtinez@gmail.com>
2024-01-25 17:44:03 -0600

    Update Signup.js

--- Signup.js
+++ Signup.js
@@ -1,10 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import './Signup.css';
+import { auth } from '../firebase';
+import { createUserWithEmailAndPassword, signOut } from 'firebase/auth';
 
 const Signup = ({ username, password, email, onLogout }) => {
-    return(
+    useEffect(() => {
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log('User signed up:', user);
+            })
+            .catch((error) => {
+                console.error('Error signing up:', error.message);
+            });
+    }, []); 
+
+    const handleLogout = () => {
+        signOut(auth)
+            .then(() => {
+                console.log('User signed out');
+                onLogout();
+            })
+            .catch((error) => {
+                console.error('Error signing out:', error.message);
+            });
+    };
+
+    return (
         <div className='signup-container-dis'>
-            <button className='logout-btn' onClick={onLogout}>Logout</button>
+            <button className='logout-btn' onClick={handleLogout}>Logout</button>
             <h2 className='signup-title-name'>Account Information</h2>
             <h3 className='signup-username'>{username}</h3>
             <h3 className='signup-email'>{email}</h3>
