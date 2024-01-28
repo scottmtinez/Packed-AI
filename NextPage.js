41f32c79bb330404e014e30dc76b237af05f40ef
Scotty <scottmtinez@gmail.com>
2024-01-27 21:42:40 -0600

    Update NextPage.js

--- NextPage.js
+++ NextPage.js
@@ -4,11 +4,25 @@ import { auth } from '../firebase';
 import { createUserWithEmailAndPassword, signOut } from 'firebase/auth';
 
 const NextPage = ({ location }) => {
+
+    const passDateToWeatherAPI = () => {
+
+    };
+
+    const passDateToFlightAPI = () => {
+    
+    };
+
     return(
         <div>
             <div className='nextPage-container'>
-                <h2>Destination Information</h2>
-                <p>Location: {location}</p>
+                <h2>{location}</h2>
+                <form className='NextPage-Form'>
+                    <input type='date' name='arrival-date' className='arrival-date '/>
+                    <input type='date' name='departure-date' className='departure-date '/><br/>
+                    <input type='text' name='extraInfo' className='extraInfo' placeholder='Extra Information about Destination...'/><br/>
+                    <input type='submit'name='submit' className='submit' value='Add Information'/>
+                </form>
             </div>
 
         </div>
