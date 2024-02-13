437671eb687e5cca7aa897ce8fdb809cd8dc931a
Scotty <scottmtinez@gmail.com>
2024-02-12 21:35:31 -0600

    Update Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -1,47 +1,72 @@
 import React, { useState, useEffect } from 'react';
 import './Dashboard.css';
 import { MdAddPhotoAlternate } from "react-icons/md";
+import { ref, onValue, off } from 'firebase/database';
+import { database } from '../firebase';
 
-function Dashboard({ username, password, email, onLogout, count}){
-    console.log('Received count:', count);
-    return(
-        <div className='dashboard-container'>
-            <button className='logout-btn' onClick={onLogout}>Logout</button><br/>
+function Dashboard({ username, password, email, onLogout, count }) {
+  const [recentLocations, setRecentLocations] = useState([]);
 
-            <div className=''>
-                <h1 className='dashboard-title'>Dashboard</h1>
-            </div>
+  useEffect(() => {
+    const recentVacationsRef = ref(database, 'recentVacations');
 
-            <div className='row1-container'>
-                <div className='num-vacations-container'>
-                    <h1 className='num-vacations-title'>{ count } Vacations</h1>
-                </div>
-                <div className='add-vacation-pictures'>
-                    <button className='add-photo-btn'><MdAddPhotoAlternate className='photo'/></button>
-                </div>
+    // Listen for changes to the data
+    onValue(recentVacationsRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        // Convert the data object into an array of locations
+        const locationsArray = Object.values(data);
+        setRecentLocations(locationsArray);
+      } else {
+        setRecentLocations([]);
+      }
+    });
 
-        
-            </div>
+    // Cleanup function to unsubscribe from database updates
+    return () => {
+      // Detach the listener when the component is unmounted
+      off(recentVacationsRef);
+    };
+  }, []); // Run effect only once when the component mounts
 
-            <div className='row2-container'>
-                <div className='recent-vacations-list'>
-                    <h1>Recent Trips</h1>
-                    <p> 
-                        No Recent Trips <br/>
-                    </p>
-                </div>
-                <div className='account-information-container'>
-                    <h1 className='account-info-title'>Account Information</h1>
-                    <h2 className='account-email'>{ username } <a href='#' className='edit'>Edit</a></h2>
-                    <h2 className='account-password'>{ password } <a href='#' className='edit'>Edit</a></h2>
-                </div>
-            </div>
+  return (
+    <div className='dashboard-container'>
+      <button className='logout-btn' onClick={onLogout}>Logout</button><br/>
 
-            <div className='row3-container'>
+      <div className=''>
+        <h1 className='dashboard-title'>Dashboard</h1>
+      </div>
 
-            </div>
+      <div className='row1-container'>
+        <div className='num-vacations-container'>
+          <h1 className='num-vacations-title'>{ count } Vacations</h1>
         </div>
-    );
+        <div className='add-vacation-pictures'>
+          <button className='add-photo-btn'><MdAddPhotoAlternate className='photo'/></button>
+        </div>
+      </div>
+
+      <div className='row2-container'>
+        <div className='recent-vacations-list'>
+          <h1>Recent Trips</h1>
+          <ul>
+            {recentLocations.map((location, index) => (
+              <li key={index}>{location.location}</li>
+            ))}
+          </ul>
+        </div>
+        <div className='account-information-container'>
+          <h1 className='account-info-title'>Account Information</h1>
+          <h2 className='account-email'>{ username } <a href='#' className='edit'>Edit</a></h2>
+          <h2 className='account-password'>{ password } <a href='#' className='edit'>Edit</a></h2>
+        </div>
+      </div>
+
+      <div className='row3-container'>
+
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
