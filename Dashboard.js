90fb7beba876fe08e976f99fd312652e169b6d51
Scotty <scottmtinez@gmail.com>
2024-02-23 14:17:58 -0600

    Update Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -4,6 +4,7 @@ import { getAuth } from 'firebase/auth';
 import { ref, onValue, off } from 'firebase/database';
 import { getDatabase } from 'firebase/database';
 
+// Function to replace "." with ","
 const replaceDotWithComma = (email) => {
   return email.replace(/\./g, ',');
 };
@@ -14,34 +15,37 @@ function Dashboard({ username, onLogout }) {
   const [recentLocations, setRecentLocations] = useState([]);
 
   useEffect(() => {
-    const auth = getAuth(); 
-    const database = getDatabase(); 
+    const auth = getAuth(); // Get the Auth instance
+    const database = getDatabase(); // Get the database instance
 
+    // Reference to the user's data in the database
     const userRef = ref(database, `users/${modifiedUsername}`);
 
+    // Listen for changes to the user's data
     const unsubscribe = onValue(userRef, (snapshot) => {
-      const userData = snapshot.val(); 
-      setUserData(userData); 
+      const userData = snapshot.val(); // Get the user's data from the snapshot
+      setUserData(userData); // Update the state with the user's data
       if (userData && userData.recentLocations) {
-        const locationsArray = Object.values(userData.recentLocations); 
-        setRecentLocations(locationsArray); 
+        const locationsArray = Object.values(userData.recentLocations); // Extract values from recentLocations object
+        setRecentLocations(locationsArray); // Update recentLocations state
       }
     });
 
+    // Clean up listener when component unmounts
     return () => {
-      off(userRef); 
+      off(userRef); // Unsubscribe from database changes
     };
   }, [modifiedUsername]);
 
   return (
     <div className="dashboard-container">
       <div className="">
-        <button className='logout-btn' onClick={onLogout}>Logout</button><br/><br/>
-        <h1 className="dashboard-title">Dashboard</h1>
+        
       </div>
 
       {userData && (
         <div className="user-data-container">
+          <button className='logout-btn' onClick={onLogout}>Logout</button><br/><br/>
           <h1 className=''>{userData.name} <a href='#' className='edit'>Edit</a></h1>
           <h2>{modifiedUsername}<a href='#' className='edit'>Edit</a></h2>
           <h2>Recent Locations:</h2>
