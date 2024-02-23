e9fbf49056cc60adfd696a3dc48b267267c9e64d
Scotty <scottmtinez@gmail.com>
2024-02-22 22:12:26 -0600

    Update NextPage.js
    Still running into some issues

--- NextPage.js
+++ NextPage.js
@@ -3,7 +3,7 @@ import './NextPage.css';
 import { auth } from '../firebase';
 import { createUserWithEmailAndPassword, signOut } from 'firebase/auth';
 import { getAuth } from 'firebase/auth';
-import { getDatabase, ref, push, set } from 'firebase/database';
+import { getDatabase, ref, push, set, child, get, runTransaction, update } from 'firebase/database';
 
 const NextPage = ({ location, username}) => {
 
@@ -22,37 +22,31 @@ const NextPage = ({ location, username}) => {
     };
 
     const sendLocationDataToFB = (location, username) => {
-        //For Testing
-            console.log("Test Location:" + location);
-            console.log("Test Username: " + username);
-        
-        // Replace "." with "," to be be read by firebase realtime db
-            const modifiedEmail = username.replace(/\./g, ',');
-            console.log("New Email: " + modifiedEmail);
-
-        // Get a reference to the Firebase Realtime Database
-            const db = getDatabase();
-            
-        // Sanitize the modified username for Firebase path
-            const sanitizedEmail = modifiedEmail.replace(/[.#$/[\]]/g, '');
-
-        // Set the user's email address and push the location data
-            const userData = {
-                email: username,
-                recentLocations: {
-                    locationId: location // Using "locationId1" as the location ID
-                }
-            };
-
-        // Set user data
-            set(ref(db, `users/${sanitizedEmail}`), userData)
-                .then(() => {
-                    console.log('Location data and email added to Firebase');
-                })
-                .catch((error) => {
-                    console.error('Error adding location data and email to Firebase:', error);
-                });
+        const auth = getAuth();
+        const user = auth.currentUser;
+    
+        if (!user) {
+            console.error('No user logged in.');
+            return;
+        }
+    
+        // Replace "." with "," and sanitize for Firebase path
+        const modifiedEmail = username.replace(/\./g, ',');
+        const sanitizedEmail = modifiedEmail.replace(/[.#$/[\]]/g, '_');
+    
+        const db = getDatabase();
+        const recentLocationsRef = ref(db, `users/${sanitizedEmail}/recentLocations`);
+    
+        // Update the recentLocations node with the new location
+        update(recentLocationsRef, { location })
+            .then(() => {
+                console.log('Location data added to Firebase under recentLocations node');
+            })
+            .catch((error) => {
+                console.error('Error adding location data to Firebase:', error);
+            });
     };
+    
 
     return(
         <div>
@@ -71,4 +65,5 @@ const NextPage = ({ location, username}) => {
 }
 
 export default NextPage;
+
 
