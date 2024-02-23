7e60d63fa57e48335f676ab27432cfc3d5e753b2
Scotty <scottmtinez@gmail.com>
2024-02-22 21:24:55 -0600

    Update NextPage.js
    Working on adding locations to a firebase list

--- NextPage.js
+++ NextPage.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import './NextPage.css';
 import { auth } from '../firebase';
 import { createUserWithEmailAndPassword, signOut } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
+import { getDatabase, ref, push, set } from 'firebase/database';
 
-const NextPage = ({ location }) => {
+const NextPage = ({ location, username}) => {
 
     const passDateToWeatherAPI = () => {
 
@@ -13,8 +15,43 @@ const NextPage = ({ location }) => {
     
     };
     
-    const sendInfo = () => {
+    
+    const sendInfo = (event) => {
+        event.preventDefault();
+        sendLocationDataToFB(location, username);
+    };
 
+    const sendLocationDataToFB = (location, username) => {
+        //For Testing
+            console.log("Test Location:" + location);
+            console.log("Test Username: " + username);
+        
+        // Replace "." with "," to be be read by firebase realtime db
+            const modifiedEmail = username.replace(/\./g, ',');
+            console.log("New Email: " + modifiedEmail);
+
+        // Get a reference to the Firebase Realtime Database
+            const db = getDatabase();
+            
+        // Sanitize the modified username for Firebase path
+            const sanitizedEmail = modifiedEmail.replace(/[.#$/[\]]/g, '');
+
+        // Set the user's email address and push the location data
+            const userData = {
+                email: username,
+                recentLocations: {
+                    locationId: location // Using "locationId1" as the location ID
+                }
+            };
+
+        // Set user data
+            set(ref(db, `users/${sanitizedEmail}`), userData)
+                .then(() => {
+                    console.log('Location data and email added to Firebase');
+                })
+                .catch((error) => {
+                    console.error('Error adding location data and email to Firebase:', error);
+                });
     };
 
     return(
@@ -25,7 +62,7 @@ const NextPage = ({ location }) => {
                     <input type='date' name='arrival-date' className='arrival-date '/>
                     <input type='date' name='departure-date' className='departure-date '/><br/>
                     <input type='text' name='extraInfo' className='extraInfo' placeholder='Extra Information about Destination...'/><br/>
-                    <input type='submit'name='submit' className='submit' value='Add Information'/>
+                    <input type='submit'name='button' className='submit' value='Add Information'/>
                 </form>
             </div>
 
@@ -34,3 +71,4 @@ const NextPage = ({ location }) => {
 }
 
 export default NextPage;
+
