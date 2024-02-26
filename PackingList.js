2dfa36cf16253824d89e470849a3a47b8054d4b5
Scotty <scottmtinez@gmail.com>
2024-02-25 22:14:26 -0600

    Update and rename List.js to PackingList.js

---
+++ PackingList.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import './PackingList.css';
+
+function PackingList(){
+    return(
+        <div>
+            <h5 className='packingList-title'>List</h5>
+
+        </div>
+    );
+}
+
+export default PackingList;
