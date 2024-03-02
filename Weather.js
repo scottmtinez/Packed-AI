8ca5db3b2cfeb8270c28e4fdb2bce21c6f725528
Scotty <scottmtinez@gmail.com>
2024-03-01 20:25:43 -0600

    Update Weather.js

--- Weather.js
+++ Weather.js
@@ -4,12 +4,12 @@ import PackingList from './PackingList';
 
 const Weather = ({ location, username, arrivalDate, departureDate, extraInfo }) => {
     const [weatherData, setWeatherData] = useState(null);
-    const KEY = 'HIDDEN';
+    const KEY = 'e56209a25977b2465f2f62739cd457b8';
 
     useEffect(() => {
         const fetchWeatherData = async () => {
             try {
-                const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${location.lat}&lon=${location.lon}&exclude=minutely,hourly&appid=${KEY}`);
+                const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=43.0761&lon=88.7743&exclude=minutely,hourly&appid=${KEY}`);
                 //const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${location.lat}&lon=${location.lon}&exclude=current,minutely,hourly&appid=${KEY}&units=metric`);
                 const data = await response.json();
                 setWeatherData(data);
@@ -37,12 +37,12 @@ const Weather = ({ location, username, arrivalDate, departureDate, extraInfo }) 
                     <button className='goToDashboardBtn'>Go to Dashboard</button>
                     <h1 className='weather-title'>{location}</h1>
                     {weatherData.daily && (
-                        <ul>
-                            {weatherData.daily.map((day, index) => (
-                                <li key={index}>
-                                    <p>Date: {formatDate(day.dt)}</p>
-                                    <p>Temperature: {day.temp.day} °C</p>
-                                    <p>Description: {day.weather[0]?.description}</p>
+                        <ul className="layout">
+                            {weatherData.daily && weatherData.daily.map((day, index) => (
+                                <li className='layout-container' key={index}>
+                                    <p className='layout-item-date'>{formatDate(day.dt)}</p>
+                                    <p className='layout-item-temp'>{day.temp.day} °C</p>
+                                    <p className='layout-item-forecast'>{day.weather[0]?.description}</p>
                                 </li>
                             ))}
                         </ul>
